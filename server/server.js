e297c69895241624a3e0319887ded7b7cfa25c61
Murial Anindo <twentymurial@outlook.com>
2018-11-24 21:15:36 -0500

    second push

---
+++ server/server.js
@@ -0,0 +1,13 @@
+var express=require ("express");
+var bodyParser=require("body-parser");
+var path=require("path");
+var exphbs=require('express-handlebars');
+
+
+var app=express();
+
+var PORT = process.env.PORT || 8000;
+
+app.listen(PORT, function(){
+	console.log("Listening on PORT " + PORT);
+});

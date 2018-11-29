ea5d2e993786d0091d47b67478c15d2cf0659f67
Murial Anindo <twentymurial@outlook.com>
2018-11-29 08:48:51 -0500

    third commit

--- server/controller/route.js
+++ server/controller/route.js
@@ -1,2 +1,11 @@
 var express=require("express");
-var path=require("path")
+var path=require("path");
+
+var router=express.Router();
+
+var user_data=require("./..models/user-data.js");
+
+router.get("/",function(req,res){
+     res.render("home",{user:user_data});
+})
+module.export=router;
---
+++ server/models/user_data.js
@@ -0,0 +1,22 @@
+module.export=[
+    {
+        name:"panther",
+        occupation:"nurse",
+        age:22,
+        bald:false
+    },
+    {
+        name:"panther",
+        occupation:"engineer",
+        age:33,
+        bald:true
+    },
+    {
+        name:"lioness",
+        occupation:"doctor",
+        age:55,
+        bald:false
+    },
+
+]
+
